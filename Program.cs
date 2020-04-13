72dd383dfde0d075b9cce59780c9d66750f8c72f
JazzLilo <63597140+JazzLilo@users.noreply.github.com>
2020-04-13 12:13:49 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fraccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resp;
+
+            Console.WriteLine("Ingrese la operacion a realizar" +
+            "\n1. Crear fracciones" +
+            "\n2. suma" +
+            "\n3. resta" +
+            "\n4. multiplicacion" +
+            "\n5. division" +
+            "\n6. simplificar" +
+            "\n7. salir");
+            fraccion z;
+            fraccion x = new fraccion(1, 4);
+            fraccion y = new fraccion(2, 4);
+            do
+            {
+                resp = Int32.Parse(Console.ReadLine());
+                switch (resp)
+                {
+                    case 1:
+                        Console.WriteLine("Procesando........");
+                        x.Leer();
+                        y.Leer();
+                        x.mostrar();
+                        y.mostrar();
+                        
+                        break;
+                    case 2:
+                        Console.WriteLine("Procesando........");
+                        z = x + y;
+                        z.mostrar();
+                        break;
+                    case 3:
+                        Console.WriteLine("Procesando........");
+                        z = x - y;
+                        z.mostrar();
+                        break;
+                    case 4:
+                        Console.WriteLine("Procesando........");
+                        z = x * y;
+                        z.mostrar();
+                        break;
+                    case 5:
+                        Console.WriteLine("Procesando........");
+                        z = x / y;
+                        z.mostrar();
+                        break;
+                    case 6:
+                        Console.WriteLine("Procesando........");
+                        int q = Int32.Parse(Console.ReadLine());
+                        int w = Int32.Parse(Console.ReadLine());
+                        z = new fraccion(q, w);
+                        z.simplificar();
+                        break;
+                    default:
+                        resp = 7;
+                        break;
+
+                }
+
+            } while (resp != 7);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ fraccion.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fraccion
+{
+    class fraccion
+    {
+        private int num, den;
+
+        public int Den
+        {
+            get { return den; }
+            set { den = value; }
+        }
+
+        public int Num
+        {
+            get { return num; }
+            set { num = value; }
+        }
+
+        public fraccion(int n, int d)
+        {
+            num = n; den = d;
+        }
+        public void mostrar()
+        {
+            Console.Write("\n La fraccion es:");
+            Console.WriteLine(num + "/" + den);
+        }
+        public static fraccion operator +(fraccion p, fraccion q)
+        {
+            int nume = (p.num * q.den) + (p.den * q.num);
+            int deno = p.den * q.den;
+            return (new fraccion(nume, deno));
+        }
+        public static fraccion operator -(fraccion p, fraccion q)
+        {
+            int nume = (p.num * q.den) - (p.den * q.num);
+            int deno = p.den * q.den;
+            return (new fraccion(nume, deno));
+        }
+        public static fraccion operator *(fraccion p, fraccion q)
+        {
+            int nume = p.num * q.num;
+            int deno = p.den * q.den;
+            return (new fraccion(nume, deno));
+        }
+        public static fraccion operator /(fraccion p, fraccion q)
+        {
+            int nume = p.num * q.den;
+            int deno = p.den * q.num;
+            return (new fraccion(nume, deno));
+        }
+        private int mcd()
+        {
+            int u = Math.Abs(num);
+            int v = Math.Abs(den);
+            if (v == 0)
+            {
+                return u;
+            }
+            int r;
+            while (v != 0)
+            {
+                r = u % v;
+                u = v;
+                v = r;
+            }
+            return u;
+        }
+        public void simplificar()
+        {
+            int dividir = this.mcd();
+            Console.WriteLine(dividir);
+            num = num / dividir;
+            den = den / dividir;
+            mostrar();
+        }
+        public void Leer()
+        {
+            num = Int32.Parse(Console.ReadLine());
+            den = Int32.Parse(Console.ReadLine());
+        }
+    }
+}
